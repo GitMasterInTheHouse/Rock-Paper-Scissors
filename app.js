f04c296367ad85366538402990326be21a13832a
webdev <n3886628@gmail.com>
2025-11-05 13:56:56 +0100

    Create getComputerChoice() function

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+let compChoice;
+function getComputerChoice() {
+  compChoice = Math.floor(Math.random() * 3) + 1;
+  switch (compChoice) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+    default:
+      return "Unknown value chosen";
+  }
+}
+
+console.log(getComputerChoice());
