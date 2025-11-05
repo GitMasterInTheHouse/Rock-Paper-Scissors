e3bbfb28cd6f2542f38e784ac4cbeefba2e1f535
webdev <n3886628@gmail.com>
2025-11-05 14:20:07 +0100

    Create logic to play one round
    Created function to get human input and function
    to play one round of rock, Paper, Scissors.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-let compChoice;
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-  compChoice = Math.floor(Math.random() * 3) + 1;
-  switch (compChoice) {
+  let randNum = Math.floor(Math.random() * 3) + 1;
+  switch (randNum) {
     case 1:
       return "rock";
     case 2:
@@ -13,4 +15,41 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let humanChoice = prompt("Enter your choice: ");
+  return humanChoice;
+}
+
+function playRound() {
+  let humanChoice = getHumanChoice().toLowerCase();
+  let computerChoice = getComputerChoice().toLowerCase();
+
+  console.log(humanChoice);
+  console.log(computerChoice);
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win!");
+    humanScore++;
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("The computer wins!");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win!");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("The computer wins!");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win!");
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("The computer wins!");
+    computerScore++;
+  } else {
+    console.log("Something went wrong. Winner undecided!");
+  }
+}
+
+playRound();
